38750a3260ab9ef8f8ebfbaa0514474a68c9c388
cosminbbx <virgacosmin@gmail.com>
2022-11-25 21:44:34 +0200

    net7

--- InvoiceXMLGenerator.Web/Controllers/HomeController.cs
+++ InvoiceXMLGenerator.Web/Controllers/HomeController.cs
@@ -15,13 +15,6 @@ namespace InvoiceXMLGenerator.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
         public IActionResult Index()
         {
